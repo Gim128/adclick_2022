6bff9731fbbbfb998fcd2fabe5d4d24c7bd552f9
Gim128 <gimhanpabasara4@gmail.com>
2022-01-26 22:29:04 +0530

    register created

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -5,6 +5,7 @@ import '../assets/validation/RegisterValidate';
 import useForm from './../screens/useForm';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { Checkbox } from '@mui/material';
+import TextField from '@mui/material/TextField';
 
 // export default function Register() {
 //     return <h1 className='register'></h1>
@@ -15,104 +16,108 @@ const Register = ({ submitForm }) => {
     const { handleChange, handleFormSubmit, values, errors } = useForm(submitForm);
 
     return(
-    <>
-        {/* <HeroSection /> */}
-        <div className={"container"} style= {{
-                backgroundImage: `url("https://ibb.co/pPCT7fH")`
-            }}>
+        <>
+            {/* <HeroSection /> */}
+            <div className={"container"} >
+                <div className='background-image'></div>
 
-                <div className='avatar'>
-                    <img  src='https://drive.google.com/file/d/1qFGl2TX55-hU4roI0lwMvU7kKXTgkvuX/view?usp=sharing'/>
+                <div className='avatar-one'>
+                    {/* <img  src='https://drive.google.com/file/d/1qFGl2TX55-hU4roI0lwMvU7kKXTgkvuX/view?usp=sharing'/> */}
                 </div>
 
                 <div className='avatar'>
                     <img  src='https://img.icons8.com/ios-filled/344/client-management.png'/>
-                </div>    
-
-            <div className='app-wrapper'>
-                <img src=',/img/reg(1).png' alt=''/>
-            </div>
-             <div className='app-wrapper-1'>
-                <img src=',/img/reg(2).png' alt=''/>
-            </div>
-            <form className='form-wrapper'>
-                <div className={"fname"}>
-                    <label className='label'>FIRST NAME :</label>
-                    <input className='input' 
-                    type="text" 
-                    name='firstname' 
-                    value={values.fname} 
-                    onChange={handleChange}
-                    />
-                    {errors.fname && <p className='error'>{errors.fname}</p>}
-                </div>
-                <div className={"lname"}>
-                    <label className='label'>LAST NAME :</label>
-                    <input 
-                    className='input'
-                    type="text" 
-                    name='lastname' 
-                    value={values.lname} 
-                    onChange={handleChange}
-                    />
-                    {errors.fname && <p className='error'>{errors.fname}</p>}
-                </div>
-                <div className={"email"}>
-                    <label className='label'>EMAIL :</label>
-                    <input className='input'
-                     type="text"
-                     name='email'
-                     value={values.email} 
-                     onChange={handleChange}
-                     />
-                     {errors.email && <p className='error'>{errors.email}</p>}
-                </div>
-                <div className={"mobile"}>
-                    <label className='label'>MOBILE NO:</label>
-                    <input className='input' 
-                    type="text" name='mobileno' 
-                    value={values.mobile} 
-                    onChange={handleChange}
-                    />
-                    {errors.mobile && <p className='error'>{errors.mobile}</p>}
-                </div>
-                <div className={"password"}>
-                    <label className='label'>PASSWORD :</label>
-                    <input className='input' 
-                    type="password" 
-                    name='pwd' 
-                    value={values.password} 
-                    onChange={handleChange}
-                    />
-                    {errors.password && <p className='error'>{errors.password}</p>}
-                </div>
-                <div className={"conpassword"}>
-                    <label className='label'>CONFIRM PASSWORD :</label>
-                    <input className='input' 
-                    type="password" 
-                    name='conpwd' 
-                    value={values.conpassword}
-                    />
-                    {errors.conpassword && <p className='error'>{errors.conpassword}</p>}
                 </div>
 
-                <FormControlLabel
-                    control = {
-                        <Checkbox 
-                            name = "checkdB"
-                            color = "primary"
+                <div className='app-wrapper'>
+                    <img src=',/img/reg(1).png' alt=''/>
+                </div>
+                <div className='app-wrapper-1'>
+                    <img src=',/img/reg(2).png' alt=''/>
+                </div>
+                <form className='form-wrapper'>
+                    <div className={"fname"}>
+                        <label className='label'>FIRST NAME :</label>
+                        <input className='input'
+                               type="text"
+                               name='firstname'
+                               value={values.fname}
+                               onChange={handleChange}
                         />
-                    }
-                    label = "I agree to the Terms & Conditions"
-                />
+                        {errors.fname && <p className='error'>{errors.fname}</p>}
+                    </div>
+                    <div className={"lname"}>
+                        <label className='label'>LAST NAME :</label>
+                        <input
+                            className='input'
+                            type="text"
+                            name='lastname'
+                            value={values.lname}
+                            onChange={handleChange}
+                        />
+                        {errors.fname && <p className='error'>{errors.fname}</p>}
+                    </div>
+                    <div className={"email"}>
+                        <label className='label'>EMAIL :</label>
+                        <input className='input'
+                               type="text"
+                               name='email'
+                               value={values.email}
+                               onChange={handleChange}
+                        />
+                        {errors.email && <p className='error'>{errors.email}</p>}
+                    </div>
+                    <div className={"mobile"}>
+                        <label className='label'>MOBILE NO:</label>
+                        <input className='input'
+                               type="text" name='mobileno'
+                               value={values.mobile}
+                               onChange={handleChange}
+                        />
+                        {errors.mobile && <p className='error'>{errors.mobile}</p>}
+                    </div>
+                    <div className={"password"}>
+                        <label className='label'>PASSWORD :</label>
+                        <input className='input'
+                               type="password"
+                               name='pwd'
+                               value={values.password}
+                               onChange={handleChange}
+                        />
+                        {errors.password && <p className='error'>{errors.password}</p>}
+                    </div>
+                    <div className={"conpassword"}>
+                        <label className='label'>CONFIRM PASSWORD :</label>
+                        <input className='input'
+                               type="password"
+                               name='conpwd'
+                               value={values.conpassword}
+                        />
+                        {errors.conpassword && <p className='error'>{errors.conpassword}</p>}
+                    </div>
 
-                <div>
-                    <button className='submit' onClick={handleFormSubmit}>REGISTER</button>
-                </div>
-            </form>
-        </div>
-        {/* <footer /> */}
-    </>
+                    <FormControlLabel
+                        control = {
+                            <Checkbox
+                                name = "checkdB"
+                                color = "primary"
+                            />
+                        }
+                        label = "I agree to the Terms & Conditions"
+                    />
+
+                    <div>
+                        <button className='submit' onClick={handleFormSubmit}>REGISTER</button>
+                    </div>
+
+                    <div>
+                        <p></p>
+                    </div>
+
+                </form>
+            </div>
+            {/* <footer /> */}
+        </>
     )
 
 };
